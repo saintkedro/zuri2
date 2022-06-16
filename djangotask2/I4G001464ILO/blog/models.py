75c79f114f4be8d4dcb163a2d4093a6586454fef
saintkedro <isongkingsley101@gmail.com>
2022-06-16 21:38:34 +0100

    Updated Model and Home page

--- djangotask2/I4G001464ILO/blog/models.py
+++ djangotask2/I4G001464ILO/blog/models.py
@@ -1,12 +1,15 @@
+#from django.conf import settings
 from django.db import models
 
 # Create your models here.
-from django.contrib.auth.models import User
-
+#from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    #author = models.ForeignKey(User, on_delete=models.CASCADE)
+    #author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    text = models.TextField()
     created_date = models.DateTimeField
     published_date = models.DateTimeField
 
